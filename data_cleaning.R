54bdb3174f676ba311057d48d3d8ae49174996ae
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2020-12-09 17:27:36 -0500

    add specific steps for analysis

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,23 +1,35 @@
 #Preliminary Analysis
 
-tox=read.csv("toxicant_clean.csv")
+tox=read.csv("prax_sheet.csv")
+str(tox)
 require(dplyr)
+tox$reference=tox$Reference..don.t.copy.
 tox$proportion=tox$OR.if.given..proportion.exposed
 tox$concentration=tox$concentration..ND.for.non.detects.
-tox$reference=tox$C
+
 
 #toxicant-specific analysis
+#Q1: p,p?-DDT and p,p?-DDE, are they same as dieldrin and DDE?
 
-#organochlorine insecticides
-length(unique(oc_tox$toxicant.specific)) #47 specific types of OCs
-oc_tox=tox%>%
-  filter(toxicant.group=="organochlorine insecticides", !is.na(proportion))%>%
+#Analysis Plan:
+
+#Step 1: identify the top identified specific toxicant per main toxicant group
+
+####heavy metals
+metal=c("metal", "heavy metals")
+
+met_tox=tox%>%
+  filter(toxicant.group %in% metal)%>%
   group_by(toxicant.specific)%>%
   summarise(count=n())%>%
   arrange(count)
-#issue: there is a blank cell
+length(unique(met_tox$toxicant.specific)) #35 specific types of heavy metals
 
-  select (reference, country, scientific.name, toxicant.group, toxicant.specific, sample.size,
-          sample.type, proportion, concentration, count)
 
-head(oc_tox)
+#Step 2: standardize values across all studies identifying/citing specific toxicant
+mercury_conc=tox%>%
+  filter(toxicant.specific=="Hg", !is.na(concentration))
+length(unique(mercury$Reference..don.t.copy.)) #3 studies
+#all seem to be in same unit of measure
+
+# Step 3: create mixed effects model for each top specific toxicant of each main tox group
