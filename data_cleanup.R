6456011910a605b88900d0143185dd0367ea48a0
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-01-23 18:49:47 -0500

    prelim plots for toxicant-specific patterns of exposure

--- data_cleanup.R
+++ data_cleanup.R
@@ -1,3 +1,5 @@
+library("readxl")
+#Read in separate xls sheets####
 Christine=read_excel("Ref_2021.xlsx", sheet="Christine")
 write.csv(Christine, "christine.csv")
 
@@ -22,6 +24,9 @@ tara=read.csv("tara.csv")
 james=read.csv("james.csv")
 sharon=read.csv("sharon.csv")
 tricia=read.csv("tricia.csv")
+
+#missing: Todd, Vince, Ariana, Pat
+#reformat orig reference sheet to teams database to add data in there?
 
 #make sure that the sheets have the same column names####
 
@@ -65,7 +70,7 @@ write.csv(full_dat, "full_data.csv")
 
 full_dat=read.csv("full_data.csv")
 
-#create subset of data to calculate proportion for those with exposed and sample size####
+#create subset of data to calculate proportion for those with no.exposed and sample size####
 full_dat_1=full_dat%>%
   filter(!is.na(no..exposed))%>%
   mutate(proportion=no..exposed/sample.size)
@@ -75,11 +80,41 @@ full_dat_2=full_dat%>%
   filter(!is.na(OR.if.given..proportion.exposed))%>%
   mutate(proportion=OR.if.given..proportion.exposed, no..exposed=proportion*sample.size)
 
-#create full data
-full_data=rbind(full_dat_1,full_dat_2)%>%
+#create full dataset
+full_dat_3=rbind(full_dat_1,full_dat_2)%>%
   mutate(exposed=no..exposed)%>%
   select(ID, Title, study.period, country, Species..English.name., subject.type,
          toxicant.specific, toxicant.group, sample.type,sample.size,exposed,proportion,
          Age.Class,Sex)
+write.csv(full_dat_3, "clean_prop_data.csv")
 
+prop_data=read.csv("clean_prop_data.csv")
 
+#data exploration
+
+tox_sam=prop_data%>%
+  group_by(sample.type, toxicant.group)%>%
+  summarise(count=n())%>%
+  arrange(count)
+#blood plasma commonest for OCs
+#eggs for FRs
+#liver for PCBs and ARs
+#tail feathers for heavy metals
+
+prop_data=prop_data%>%
+  mutate(toxicant.group=recode(toxicant.group, "pesticides"="organochlorine insecticides"))%>%
+  filter(!(toxicant.group=="unknown"))%>%
+  mutate(Age=recode(Age.Class,"adult"="2", "Adult"="2", "SY"="2", "ASY"="2", "HY"="1", "egg(s)"="1", 
+                    "juvenile"="1", "unknown"="2", "unknown/mixed"="2"))
+
+graph <- ggplot(prop_data, aes(x = toxicant.group, y = proportion)) +
+  geom_boxplot() +
+  geom_jitter(size = 1, alpha = 0.5, width = 0.25, colour = 'black') +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+graph
+=
